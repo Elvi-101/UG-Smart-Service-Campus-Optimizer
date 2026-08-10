a01b3304051a79a6cbb69b09372ba1fe37266e77
Elvis Boateng <elvisboateng557@gmail.com>
2026-08-09 21:10:43 -1200

    chore: initialize project structure

---
+++ src/main/java/campusoptimizer/Main.java
@@ -0,0 +1,11 @@
+package main.java.campusoptimizer;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("==========================================");
+        System.out.println("UG SMART SERVICE OPERATIONS OPTIMIZER");
+        System.out.println("==========================================");
+        System.out.println("System starting...");
+    }
+}
